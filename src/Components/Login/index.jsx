80125d32d55e94d4483485a2bb6c9f2da6c13b8e
Pontes <hpo.pontes@gmail.com>
2023-11-19 12:20:34 -0300

    Voltar para o Register

--- src/Components/Login/index.jsx
+++ src/Components/Login/index.jsx
@@ -3,78 +3,93 @@ import { Alert } from "react-bootstrap";
 import Home from "../HomeLog";
 
 function Login() {
-  const [emaillog, setEmaillog] = useState("");
-  const [passwordlog, setPasswordlog] = useState("");
-
-  const [flag, setFlag] = useState(false);
-  const [home, setHome] = useState(true);
-
-  async function handleLogin(e) {
-    e.preventDefault();
-
-    try {
-      const response = await fetch("http://localhost:3001/usuarios");
-      const data = await response.json();
-
-      const user = data.find(
-        (u) => u.email === emaillog && u.password === passwordlog
-      );
-
-      if (!user) {
-        setFlag(true);
-      } else {
-        // Armazenar informações na sessão
-        sessionStorage.setItem("userEmail", user.email);
-        sessionStorage.setItem("userName", user.name);
+    const [emaillog, setEmaillog] = useState("");
+    const [passwordlog, setPasswordlog] = useState("");
   
-        setHome(!home);
-        setFlag(false);
+    const [flag, setFlag] = useState(false);
+    const [home, setHome] = useState(true);
+  
+    const handleLogin = async (e) => {
+      e.preventDefault();
+  
+      try {
+        const response = await fetch("http://localhost:3001/usuarios");
+        const data = await response.json();
+  
+        const user = data.find(
+          (u) => u.email === emaillog && u.password === passwordlog
+        );
+  
+        if (!user) {
+          setFlag(true);
+        } else {
+          // Armazenar informações na sessão
+          sessionStorage.setItem("userEmail", user.email);
+          sessionStorage.setItem("userName", user.name);
+  
+          setHome(!home);
+          setFlag(false);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
+    };
+  
+    const handleGoToRegister = () => {
+      // Redirecionar para a tela de registro
+      window.location.href = '/register'; // ou use o react-router-dom para navegação programática
+    };
+  
+    return (
+      <div>
+        {home ? (
+          <form onSubmit={handleLogin}>
+            <h3>LogIn</h3>
+            <div className="form-group">
+              <label>Email</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
+                onChange={(event) => setEmaillog(event.target.value)}
+              />
+            </div>
+  
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+                onChange={(event) => setPasswordlog(event.target.value)}
+              />
+            </div>
+  
+            <button type="submit" className="btn btn-dark btn-lg btn-block">
+              Login
+            </button>
+  
+            {flag && (
+              <Alert color="primary" variant="warning">
+                Fill correct Info else keep trying.
+              </Alert>
+            )}
+  
+            {/* Botão para ir para a tela de registro */}
+            <button
+              type="button"
+              className="btn btn-link"
+              onClick={handleGoToRegister}
+            >
+              Go to Register
+            </button>
+          </form>
+        ) : (
+          <Home />
+        )}
+      </div>
+    );
   }
-
-  return (
-    <div>
-      {home ? (
-        <form onSubmit={handleLogin}>
-          <h3>LogIn</h3>
-          <div className="form-group">
-            <label>Email</label>
-            <input
-              type="email"
-              className="form-control"
-              placeholder="Enter email"
-              onChange={(event) => setEmaillog(event.target.value)}
-            />
-          </div>
-
-          <div className="form-group">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Enter password"
-              onChange={(event) => setPasswordlog(event.target.value)}
-            />
-          </div>
-
-          <button type="submit" className="btn btn-dark btn-lg btn-block">
-            Login
-          </button>
-
-          {flag && (
-            <Alert color="primary" variant="warning">
-              Fill correct Info else keep trying.
-            </Alert>
-          )}
-        </form>
-      ) : (
-        <Home />
-      )}
-    </div>
-  );
-}
+  
 
 export default Login;
